f4dc6c5fbd3b4957d28fce1de9124dc1fdf43bde
cyrilshanway <cyrilshanway@gmail.com>
2014-11-13 14:40:40 +0800

    Initial Commit

---
+++ pageControllers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pageControllers
+//
+//  Created by Cyrilshanway on 11/13/14.
+//  Copyright (c) 2014 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pageControllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pageControllers
+//
+//  Created by Cyrilshanway on 11/13/14.
+//  Copyright (c) 2014 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
